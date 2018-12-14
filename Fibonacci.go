75088fe9b940007f535a9fdf7a0e4b88a9d7006d
Ishaq Idris <sharkyferrari@gmail.com>
2018-12-14 14:00:33 +1300

    added fibonacci excercise

---
+++ Fibonacci.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	first, second := 0, 1
+	fmt.Printf("First: %d , Second: %d\n", first, second)
+	return func() int {
+		first, second = second, first+second
+		return first
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
